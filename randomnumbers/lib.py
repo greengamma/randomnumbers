374bbeb0fcda435e2f241b4ddba5affe45af73d8
Rene Kumsta <rck@sequensis.de>
2022-07-30 12:21:44 +0200

    Initial Push

---
+++ randomnumbers/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    import random
+    random_number = random.randint(1, 10)
+    print(f'The random number between 1 and 10 is: {random_number}')
+
+if __name__ == "__main__":
+    result = try_me()
+    print(result)
